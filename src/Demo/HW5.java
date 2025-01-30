4a0462ea4190e1d19264b1c1b26a97eef9034cb2
chuchu <chuchu@rands-air.lan>
2025-01-30 10:40:21 -0500

    new code implemnted

---
+++ src/Demo/HW5.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class HW5 {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		int result = add(5, 3);
+		System.out.println("Sum: " + result);
+	}
+}
