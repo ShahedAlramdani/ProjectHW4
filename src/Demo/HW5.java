987ce5e72709340b9770d32d522af351f6b7425f
Shahed <Shahedalramdani@gmail.com>
2025-01-30 10:58:15 -0500

    Update HW5.java
    new logic

--- src/Demo/HW5.java
+++ src/Demo/HW5.java
@@ -10,4 +10,14 @@ public class HW5 {
 		int result = add(5, 3);
 		System.out.println("Sum: " + result);
 	}
+	public static int add(int m, int v) {
+		return m + v;
+	}
+
+	public static void main(String[] args) {
+		int result = add(8, 5);
+		System.out.println("Sum: " + result);
+	}
+
+	
 }
